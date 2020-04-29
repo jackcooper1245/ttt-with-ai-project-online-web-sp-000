9924abcc06da69ee30b689dace900fbc9c7f0180
Jack Cooper <jackcooper1245@gmail.com>
2020-04-29 19:04:49 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,7 @@ class Game
   [2,5,8],
   [0,4,8],
   [2,4,6] ]
-  
+
   def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
     @player_1 = player_1
     @player_2 = player_2
